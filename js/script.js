f418327855b7d3924642d71669af0d5a6671a3a7
daniel alves <11daniel.neves.alves@gmail.com>
2023-02-18 22:05:44 -0300

    Ajuste na função showSection Script JS

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ var typed = new Typed(".typing", {
 /* ===================================== Aside ===================================== */
 const nav = document.querySelector(".nav"),
       navList =  nav.querySelectorAll("li")
-      totalNavList = navList.length
+      totalNavList = navList.length,
+      allSection = document.querySelectorAll(".section"),
+      totalSection = allSection.length;
       for(let i=0; i<totalNavList; i++)
       {
         const a = navList[i].querySelector("a");
@@ -23,4 +25,12 @@ const nav = document.querySelector(".nav"),
             showSection(this);
         })
       }
-      function showSection(){}
+      function showSection(element)
+      {
+        for(let i = 0; i < totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active");
+      }
